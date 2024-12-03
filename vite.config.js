2b5d9f354de2f94cd90b08b7c159921a276079ea
AlexBout2 <a.boutin@cci-formation.nc>
2024-12-04 10:15:35 +1100

    Config git hub pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 let mypath = `/${path.basename(__dirname)}/dist/`
 
 export default defineConfig({
+    // base : "/Gw2_Compagnon_app"
     base: `/${path.basename(__dirname)}/dist/`, 
     plugins: [vue()],
     build :{
