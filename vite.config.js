19ad3656d405e8d4dbfec7b001b0083f7f14e3a4
Alex <boutalexis@gmail.com>
2024-12-06 13:14:02 +1100

    Ajout Dist pour prod

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+    const env = loadEnv(mode, process.cwd());
 
-  return defineConfig({
-    base: env.VITE_BASE ||'/',
-    plugins: [vue()],
-    build: {
-      watch: {
-        exclude: [
-          'dist/**',
-        ]
-      }
-    }
-  });
+    return defineConfig({
+        base: env.VITE_BASE || "/",
+        plugins: [vue()],
+        build: {
+            watch: {
+                exclude: ["dist/**"],
+            },
+        },
+    });
 };
